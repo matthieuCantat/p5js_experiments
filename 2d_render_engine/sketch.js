7bf9fb27d739dfa4b1625191a8797e791e5bee6f
matthieu cantat <matthieucantat@gmail.com>
2025-06-04 23:13:53 +0200

    update

--- 2d_render_engine/sketch.js
+++ 2d_render_engine/sketch.js
@@ -224,179 +224,270 @@ class rectangle
 // Declare the boolean value as a global variable
 class User_interaction_info
 {
+	LOG_LISTENERS = false
     constructor()
     {
-        this.isMousePressed = false
-        this.isScreenTouched = false
-        this.userIsInteracting = false
-        this.userIsInteracting_last = null
-        this.userInteractionChange = false
-        this.mouseX = 0
-        this.mouseY = 0
+		this.ON_COMPUTER = true
+
+		this.isPressed_draw_debug = {
+			count: 0,
+			max_count: 100,
+		}
+		this.isReleased_draw_debug = {
+			count: 0,
+			max_count: 100,
+		}
+
+		//
+        this._isMousePressed = false
+        this._isScreenTouched = false
+        this._isInteracting_last = null
+
         this._eventTimer = null;
 
+		// INTERACTION
+		this.isInteracting = false
+		this.isPressed = false
+		this.isReleased = false
+		this.interactionChanged = false
+		this.isInteractingCount = 0
+		this.isNotInteractingCount = 0
+
 		this.p = null
-		this.pStart = null
-		this.pEnd = null
+		this.p_last = null
+		this.pReleased = null
+		this.pPressed = null
+
 		this.trailPoints = []
 		this.trailPoints_nbrMax = 200
 		for( let i = 0; i < this.trailPoints_nbrMax; i++)
-		{
 			this.trailPoints.push(null)
-		}
 
-		this.ON_COMPUTER = true
+		// OBJ TO INTERACT
+		this.something_is_selected = false
+		this.selection_info = { obj : null , vOffset : null }
+		
     }
+
+    handleMouseDown(event)
+	{
+        if(this.LOG_LISTENERS)console.log('handleMouseDown')
+        this.isInteracting = true
+		this.add_mouse_coords( event.clientX, event.clientY)
+    }
+
+    handleTouchDown(event)
+	{
+        if(this.LOG_LISTENERS)console.log('handleTouchDown')
+        this.isInteracting = true
+        const touch = event.touches[0] || event.changedTouches[0]; 
+		this.add_mouse_coords( touch.clientX, touch.clientY)
+    }	
+    
+    handleMouseUp(event) {
+        if(this.LOG_LISTENERS)console.log('handleMouseUp')
+        this.isInteracting = false
+		this.p = null
+    }
+
+    handleTouchUp(event) {
+        if(this.LOG_LISTENERS)console.log('handleTouchUp')
+        this.isInteracting = false
+		this.p = null
+    }   
+    
+    handleMouseMove(event) 
+	{
+		if(this.LOG_LISTENERS)console.log('handleMouseMove')
+		if( ( this.ON_COMPUTER )&&(this.isInteracting == false) )
+			return 
+		this.add_mouse_coords( event.clientX, event.clientY)
+    }	
+    handleTouchMove(event)
+    {
+		if(this.LOG_LISTENERS)console.log('handleTouchMove')
+		const touch = event.touches[0] || event.changedTouches[0];      
+		this.add_mouse_coords( touch.clientX, touch.clientY)
+    }
+
+	// LISTENERS
+	addEventListeners(doc)
+	{
+		doc.addEventListener('mousedown',  (event) => {this.handleMouseDown(event)});
+		doc.addEventListener('mouseup',    (event) => {this.handleMouseUp(event)});
+		doc.addEventListener('mousemove',  (event) => {this.handleMouseMove(event)});
+		doc.addEventListener('touchstart', (event) => {this.handleTouchDown(event)});
+		doc.addEventListener('touchend',   (event) => {this.handleTouchUp(event)});
+		doc.addEventListener('touchmove',  (event) => {this.handleTouchMove(event)}); 
+	}
 
 	trail_clear()
 	{
 		this.trailPoints = []
 		for( let i = 0; i < this.trailPoints_nbrMax; i++)
-		{
 			this.trailPoints.push(null)
-		}
 	}	
 	
 	trail_add(p)
 	{
 		for( let i = this.trailPoints_nbrMax-1; 0 < i; i--)
-		{
 			this.trailPoints[i] = this.trailPoints[i-1]
-		}
 		this.trailPoints[0] = p
 	}
 
+	add_mouse_coords(x,y)
+	{
+		this.p = new Vector( cX_inv( { x : x} ), cY_inv( { y : y} ) )
+	}
 
-    handleMouseDown(event) {
-        console.log('handleMouseDown')
-        this.isMousePressed = true;
-        this.userIsInteracting_last = this.userIsInteracting
-        this.userIsInteracting = this.isScreenTouched || this.isMousePressed
-        if( this.userIsInteracting != this.userIsInteracting_last)
-            this.userInteractionChange = true
-        else
-        this.userInteractionChange = false
-        //console.log('Mouse is pressed:', user_interaction_info.isMousePressed);
-        this.user_idle_check()
-    }
-    
-    handleMouseUp(event) {
-        console.log('handleMouseUp')
-        this.isMousePressed = false;
-      
-        this.userIsInteracting_last = this.userIsInteracting
-        this.userIsInteracting = false
-        if( this.userIsInteracting != this.userIsInteracting_last)
-            this.userInteractionChange = true
-        else
-            this.userInteractionChange = false
-        //console.log('Mouse is pressed:', user_interaction_info.isMousePressed);
-        this.user_idle_check()
+	add_selection_info(obj)
+	{
+		this.selection_info.obj = obj
+		this.selection_info.vOffset = this.pPressed.getMult( obj.m.getInverse())
+	}
 
-		this.p = null
-		this.trail_clear()
-    }
+	clear_selection_info()
+	{
+		this.selection_info.obj = null
+		this.selection_info.vOffset = null
+	}
 
-    // Function to handle the mousemove event
-    handleMouseMove(event) {
-		if( ( this.ON_COMPUTER )&&(this.isMousePressed == false) )
-			return 
-		console.log('handleMouseMove')
-		this.mouseX = cX_inv( { x : event.clientX} )
-		this.mouseY = cY_inv( { y : event.clientY} )		
-		this.p = new Vector(this.mouseX,this.mouseY)
-		this.trail_add(this.p)
-    }
-	
+	update_state()
+	{
+		this.interactionChanged = false
+		this.isPressed = false
+		this.isReleased = false
+		if( this.isInteracting != this._isInteracting_last)
+		{
+			this.interactionChanged = true
+			if(this.isInteracting == true)
+				this.isPressed = true
+			else
+				this.isReleased = true
+		}
+     
+		this._isInteracting_last = this.isInteracting
+	}
 
-    handleTouchDown(event) {
-        console.log('handleTouchDown')
-        this.isScreenTouched = true;
-        this.userIsInteracting_last = this.userIsInteracting
-        this.userIsInteracting = this.isScreenTouched || this.isMousePressed
-        if( this.userIsInteracting != this.userIsInteracting_last)
-            this.userInteractionChange = true
-        else
-        this.userInteractionChange = false  
-        //console.log('Mouse is pressed:', this.isMousePressed);
-        // mouse must be update sooner
-        const touch = event.touches[0] || event.changedTouches[0]; 
+	update_counter()
+	{
+		if(this.isInteracting)
+		{
+			this.isInteractingCount +=1
+			this.isNotInteractingCount = 0
+		}
+		else
+		{
+			this.isInteractingCount = 0
+			this.isNotInteractingCount += 1
+		}
+	}
 
-		this.mouseX = cX_inv( { x : touch.clientX} )
-		this.mouseY = cY_inv( { y : touch.clientY} )
-		this.p = new Vector(this.mouseX,this.mouseY)
-		this.trail_add(this.p)
-		this.pStart = this.p
+	update_coords()
+	{
+		if( this.isPressed)
+			this.pPressed = this.p
 
-        this.user_idle_check()  
-    }
+		if(this.isReleased)
+			this.pReleased = this.p_last
+			
 
-    handleTouchUp(event) {
-        console.log('handleTouchUp')
-        this.isScreenTouched = false;
-    
-        this.userIsInteracting_last = this.userIsInteracting
-        this.userIsInteracting = false
-        if( this.userIsInteracting != this.userIsInteracting_last)
-            this.userInteractionChange = true
-        else
-        this.userInteractionChange = false
-        
-        this.user_idle_check()
-		this.p = null
-		this.trail_clear()
-    }   
-    
+		if(this.isInteracting)
+		{
+			this.trail_add(this.p)
+			this.pReleased = null
+		}
+		else
+		{
+			this.trail_clear()
+			this.pPressed = null
+		}
+			
+		this.p_last = this.p
+	}
 
-    handleTouchMove(event)
-    {
-	  console.log('handleTouchMove')
-      const touch = event.touches[0] || event.changedTouches[0];
-      
-	  this.mouseX = cX_inv( { x : touch.clientX} )
-	  this.mouseY = cY_inv( { y : touch.clientY} )
-	  this.p = new Vector(this.mouseX,this.mouseY)
-	  this.trail_add(this.p)
-
-      //console.log('Mouse is pressed:', user_interaction_info.isMousePressed);
-    }
-
-
-    // Reset the timer whenever an event is detected
-    user_idle_check()
-    {
-        // CLEAR EXECUTION COUNTER
-        clearTimeout(this._eventTimer);
-
-        // SETUP EXECUTION COUNTER
-        let waiting_time_before_execution = 50; // == 5 seconde
-        this._eventTimer = setTimeout( 
-            () => {this.user_do_idle }, 
-            waiting_time_before_execution);
-    }
-
-    user_do_idle()
-    {
-        if( this.userInteractionChange == true)
-        {
-            this.userIsInteracting_last = this.userIsInteracting
-            this.userInteractionChange = false 
-        } 
-    }
 
 	draw()
 	{
-		if( this.isMousePressed)
-		{
+		this.update_state()
+		this.update_counter()
+		this.update_coords()
 		
-			let p = new Vector( this.mouseX, this.mouseY)
+		/*
+		if( true )
+		{
+			console.log('==============================')
+			console.log('isInteracting',this.isInteracting)
+			//console.log('isInteracting_last',this._isInteracting_last)
+			console.log('interactionChanged',this.interactionChanged)
+			console.log('isPressed',this.isPressed)
+			console.log('isReleased',this.isReleased)
+		}
+		*/
+		
+		
+		
+		if((0<this.isInteractingCount)&&(this.isInteractingCount<50)&&(this.pPressed!= null))
+		{	
 			
-			draw_circle( p.v,
+			draw_circle( this.pPressed.v,
+				Math.max( 30, 70 -this.isInteractingCount*5),
+				'red',
+				'back',
+				5)
+
+
+		}
+		
+		if((this.isNotInteractingCount)&&(this.isNotInteractingCount<50)&&(this.pReleased!= null))
+		{
+			draw_circle( this.pReleased.v,
+				Math.max( 30, 70 -this.isNotInteractingCount*5),
+				'blue',
+				'back',
+				5)		
+
+		}
+
+		if( this.isInteracting)
+		{
+			// CURRENT MOUSE PRESSED
+			draw_circle( this.p.v,
 						10,
 						'red',
 						'back',
 						5)
 
+			
+			// SELECTED OBJ SELECTION
+			if( this.selection_info.obj != null)
+			{
+				let m = this.selection_info.obj.m
+				let v = this.selection_info.vOffset
+				let p = v.getMult(m)
+				draw_circle( p.v,
+					10,
+					'yellow',
+					'back',
+					5)
+
+				let m_p = m.get_row(2)
+				draw_circle( m_p.v,
+					10,
+					'yellow',
+					'back',
+					5)				
+					
+				daw_line([p.v,m_p.v],
+						'yellow',
+						2,)
+
+				daw_line([p.v,this.p.v],
+						'red',
+						2,)						
+			}
+
+			// TRAIL
 			let points = []
 			for( let i = 0; i < this.trailPoints_nbrMax; i++)
 			{
@@ -411,6 +502,14 @@ class User_interaction_info
 				2,
 			)
 
+			this.isPressed_draw_debug.count += 1
+			this.isReleased_draw_debug.count = 0
+		}
+		else
+		{
+			this.isPressed_draw_debug.count = 0
+			this.isReleased_draw_debug.count += 1
+
 		}
 	}
 
@@ -421,7 +520,7 @@ class User_interaction_info
 
 
 var draw_elements = []
-var user_interaction_info = new User_interaction_info();
+var user_interaction = new User_interaction_info();
 
 function setup()
 {
@@ -436,6 +535,50 @@ function setup()
 		draw_elements.push( new rectangle( {m : m, color: COLORS[color_index]})  )
 	}
 }
+
+function update()
+{
+		
+	for( let elem of draw_elements )
+		elem.m.rotate(0.01)
+	
+
+
+	if( user_interaction.p != null  )
+	{
+		if(( user_interaction.something_is_selected == false )&&(user_interaction.isPressed == true))
+		{
+					
+			for( let elem of draw_elements )
+				elem.isSelected = false
+				
+			for( let i = draw_elements.length -1 ; 0 <= i; i-- )
+			{
+				if( draw_elements[i].isPointInside( user_interaction.p.v.x, 
+													user_interaction.p.v.y) )
+				{
+					draw_elements[i].isSelected = true
+					user_interaction.something_is_selected = true
+					user_interaction.add_selection_info(draw_elements[i])
+					break
+				}
+			}
+		}
+	}
+	
+
+	if( user_interaction.isInteracting == false )
+	{
+		user_interaction.something_is_selected = false
+		user_interaction.clear_selection_info()
+		for( let elem of draw_elements )
+			elem.isSelected = false		
+	}
+
+
+
+}	
+
 
 var draw_count = 0;
 function draw() {
@@ -453,23 +596,6 @@ function draw() {
 		y:Math.sin(draw_count*0.02)*200
 	}
 
-	for( let elem of draw_elements )
-		elem.m.rotate(0.01)
-
-	for( let elem of draw_elements )
-		elem.isSelected = false
-
-	
-	if( user_interaction_info.p != null )
-	{
-		for( let i = draw_elements.length -1 ; 0 < i; i-- )
-			if( draw_elements[i].isPointInside( user_interaction_info.p.v.x, 
-												user_interaction_info.p.v.y) )
-			{
-				draw_elements[i].isSelected = true
-				break
-			}
-	}
 	
 
 	for( let elem of draw_elements )
@@ -484,39 +610,31 @@ function draw() {
 		5,
 	)		
 
-	user_interaction_info.draw()
+	user_interaction.draw()
 
 
 	draw_count += 1
-	requestAnimationFrame(draw);
 }
 
+function game_loop()
+{
+	update()
+	draw()
+	requestAnimationFrame(game_loop);
+}
 
 
 
 
 // Attach event listeners when the document is fully loaded
 window.onload = function() {
-    canvas.addEventListener('mousedown', (event) => {user_interaction_info.handleMouseDown(event)});
-    canvas.addEventListener('mouseup', (event) => {user_interaction_info.handleMouseUp(event)});
-    canvas.addEventListener('mousemove', (event) => {user_interaction_info.handleMouseMove(event)});
-    canvas.addEventListener('touchstart', (event) => {user_interaction_info.handleTouchDown(event)});
-    canvas.addEventListener('touchend', (event) => {user_interaction_info.handleTouchUp(event)});
-    canvas.addEventListener('touchmove', (event) => {user_interaction_info.handleTouchMove(event)});    
+	user_interaction.addEventListeners(canvas)   
 }
 ////////////////////////////////////////////////// mouse pressed
 
-/*
-var user_interaction_info = {}
-canvas.addEventListener('click', (event) => {
-	const rect = canvas.getBoundingClientRect();
-	const x = event.clientX - rect.left;
-	const y = event.clientY - rect.top;
-}
-	*/
 
 setup();
-draw();	
+game_loop();	
 
 
 
