b2213377b633c99e5f57a8b8c0bd8777e8f546e6
matthieu cantat <matthieucantat@gmail.com>
2025-06-16 22:48:45 +0200

    redius rotation

--- 2d_render_engine/sketch.js
+++ 2d_render_engine/sketch.js
@@ -97,7 +97,7 @@ function update()
 	// CONSTRAINT
 	let rotateConstraint_A = { objs:[ draw_elements[0], draw_elements[1]], attrs:[ 'r', 'r'], mult: 2 }
 	let rotateConstraint_B = { objs:[ draw_elements[0], draw_elements[2]], attrs:[ 'r', 'r'], mult:-3 }
-	let rotateConstraint_C = { objs:[ draw_elements[3], draw_elements[4]], attrs:[ 'ty', 'r'], mult:-2 }
+	let rotateConstraint_C = { objs:[ draw_elements[3], draw_elements[4]], attrs:[ 'ty', 'r'], mult:-0.03 }
 	if( (user_interaction.something_is_selected )&&(user_interaction.isInteracting))
 	{
 		if( rotateConstraint_A.objs[1] == user_interaction.selection_info.obj)
@@ -135,7 +135,7 @@ function update()
 		value = rotateConstraint_C.objs[0].m.get_row(2).x
 	else if( rotateConstraint_C.attrs[0] == 'ty')
 		value = rotateConstraint_C.objs[0].m.get_row(2).y
-	value *= rotateConstraint_B.mult
+	value *= rotateConstraint_C.mult
 
 	if( rotateConstraint_C.attrs[1] == 'r')
 		rotateConstraint_C.objs[1].m.setRotation(value)
